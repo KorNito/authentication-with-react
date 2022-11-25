cb8052a9f7a83b1512648d5b2323a35d3c4b7060
KorNito <kornelijuspacevicius@gmail.com>
2022-11-25 23:44:35 +0200

    Saving token duration time to local storage

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -63,7 +63,11 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        authContext.login(data.idToken);
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+
+        authContext.login(data.idToken, expirationTime.toISOString());
         history.replace("/");
       })
       .catch((err) => {
