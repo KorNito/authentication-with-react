eed38a42c30b2c1fced8de85867065637ce22fa9
KorNito <kornelijuspacevicius@gmail.com>
2022-11-25 21:43:32 +0200

    Login request

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -21,25 +21,31 @@ const AuthForm = () => {
 
     setIsLoading(true);
 
+    let url;
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAyejV5ktTK-0-D0l48tHpZxdrDOqW7f9Q";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAyejV5ktTK-0-D0l48tHpZxdrDOqW7f9Q",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAyejV5ktTK-0-D0l48tHpZxdrDOqW7f9Q";
+    }
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
 
         if (res.ok) {
+          return res.json();
         } else {
           return res.json().then((data) => {
             let errorMessage = "Authentication failed";
@@ -47,11 +53,16 @@ const AuthForm = () => {
               errorMessage = data.error.message;
             }
 
-            alert(errorMessage);
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
 
   return (
